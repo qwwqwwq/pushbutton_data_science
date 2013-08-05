3e7d624fc3286c05baa7705fe34acac011f8b954
Jeffrey Quinn <jeffreyquinn@Jeffreys-MacBook-Air.local>
2013-08-05 12:54:34 -0700

    More bug fixes after testing on real use case.

--- combine_tables.py
+++ combine_tables.py
@@ -1,17 +1,48 @@
 import pandas
+from collections import Counter
 import sys
 import math
 from bhattacharyya import best_mapping
 import argparse
+import re
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Combine N spreadsheets into one intelligently.')
 parser.add_argument('filenames', type=str, nargs='+',
     help="Input csv filenames.")
 parser.add_argument('--use-labels', dest='use_labels',action='store_true', default=False,
     help="Don't examine two dataseries if they have the same name, assume they are the same.")
+parser.add_argument('--coerce-numeric', dest='coerce_numeric',action='store_true', default=False,
+    help="If a series contains strings which appear to be numeric, coerce to a numeric series.")
 parser.add_argument('--outfile', dest='outfile', type=str, required = True,
     help="Output csv.")
 args = parser.parse_args()
+
+def coerce_numeric(v):
+    if isinstance(v, basestring):
+	stripv = v.strip()
+	try:
+	    float(v)
+	except ValueError:
+	    return None
+	return stripv
+    else:
+	if np.isnan(v): return None
+	raise TypeError("Non-string object detected in df {t}.".format(t = type(v)))
+
+def coerce_numeric_series(df):
+    for key in df.keys():
+	nclasses = len(Counter(df[key]))
+	if nclasses-1 <= (len(df[key])/20)+1: continue
+	if df[key].dtype == np.dtype('O'):
+	    coerced = df[key].apply(coerce_numeric)
+	    coerced = coerced.astype(float)
+	    if coerced.count() < (len(coerced) * float(0.50)):
+		print key, "failed to be made numeric", coerced.count(), len(coerced)
+		continue
+	    else:
+		print key, "made numeric"
+		df[key] = coerced
 
 def combine_dfs(dfs, use_labels=False):
     dfs.sort(key = lambda x: len(x.index) )
@@ -21,16 +52,35 @@ def combine_dfs(dfs, use_labels=False):
 	name_map, data_map = best_mapping(df1, df2)
 	for k,v in data_map.iteritems():
 	    if use_labels and (k in df1): continue
-	    df2[k] = df2[k].apply(lambda x: v[x])
+	    if len(Counter(df2[k])) == len(Counter(df1[name_map[k]])):
+#		print "------------------------------------------------------------"
+#		print len(Counter(df2[k])) < (len(df2[k])/2) and len(Counter(df1[name_map[k]])) < (len(df1[name_map[k]])/2)
+#		print len(Counter(df2[k])), len(df2[k])
+#		print len(Counter(df1[name_map[k]])),  (len(df1[name_map[k]])/2)
+#		print k
+#		print v
+#		print df2[k]
+#		print df1[name_map[k]]
+		df2[k] = df2[k].apply(lambda x: v[x])
+	    else:
+		name_map[k] = k
 	for k in name_map.keys():
 	    if args.use_labels and (k in df1):
 		name_map[k] = k
+	print name_map
 	df2.rename( columns=name_map, inplace=True)
+	df1.to_csv("test1")
+	df2.to_csv("test2")
+
 	dfs.append(pandas.concat([df1, df2]))
+    print name_map
     return dfs.pop()
 
 if __name__ == '__main__':
     dfs=[]
     for fn in args.filenames:
 	dfs.append( pandas.io.parsers.read_csv(fn) )
+    if args.coerce_numeric:
+	for df in dfs:
+	    coerce_numeric_series(df)
     combine_dfs(dfs, use_labels = args.use_labels ).to_csv(args.outfile, index=False)
