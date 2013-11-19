909c84596935fe285b98d7e4a7e78d9488c018a4
jquinn <jquinn@castlighthealth.com>
2013-11-18 17:20:47 -0800

    finalized

--- determine_header.py
+++ determine_header.py
@@ -24,7 +24,6 @@ class DetermineHeader(object):
 
     def process_file(self, data):
 	f = [ line for (idx,line) in enumerate(data.splitlines()) if idx < self.sample_size ]
-	print f
 	self.num_lines = len(f)
 	for idx, line in enumerate(f):
 	    first_line_freqs = defaultdict(lambda: 0)
@@ -36,20 +35,16 @@ class DetermineHeader(object):
 		    self.update_freqs(other_line_freqs, line)
 	    if idx == 0:
 		self.first_metric = bc_discrete_w(first_line_freqs, 1, other_line_freqs, (self.num_lines - 1))
-		print self.first_metric
 	    self.bcs.append(bc_discrete_w(first_line_freqs, 1, other_line_freqs, (self.num_lines - 1)))
-	print self.bcs
 	self.bcs.sort()
-	print self.bcs
 
     def update_freqs(self, dest, line):
 	for char, appearances in Counter(line).iteritems():
 	    dest[char] += appearances
 
     def is_first_line_a_header(self):
-	print self.bcs.index(self.first_metric)
-	if self.bcs.index(self.first_metric) >= ((len(self.bcs)-1) * 0.95): return True
-	else: return False
+	if self.bcs.index(self.first_metric) >= ((len(self.bcs)-1) * 0.95): return 1
+	else: return 0
 
 
 if __name__ == '__main__':
